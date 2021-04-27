33a3de275a33e4536ba25f21fec44d04e80c0745
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2021-04-27 14:34:21 +0100

    chore: Added coord helper

--- Data/Scripts/Accelerometer/AccelWidget.cs
+++ Data/Scripts/Accelerometer/AccelWidget.cs
@@ -20,6 +20,7 @@ using System.Collections.Generic;
 using System.Text;
 using Digi;
 using Draygo.API;
+using Sandbox.Game;
 using Sandbox.ModAPI;
 using VRage.Utils;
 using VRageMath;
@@ -28,6 +29,23 @@ using BlendTypeEnum = VRageRender.MyBillboard.BlendTypeEnum;
 namespace Natomic.Accelerometer
 {
 
+    /// <summary>
+    /// Contains functions to transform the 1, -1 -> -1, 1 coordinate system into a sane 0, 0 -> 1, 1 
+    /// </summary>
+    static class CoordHelper
+    {
+
+        public static Vector2D SaneToKeenCoord(Vector2D sane)
+        {
+            var scaled = sane * 2;
+            return new Vector2D(-1 * (1 - scaled.X), 1 - scaled.Y);
+        } 
+        public static Vector2D KeenCoordToSane(Vector2D keen)
+        {
+            // keen.kill()
+            return new Vector2D(1 + (keen.X / 2 - 0.5), 1 - (keen.Y / 2 + 0.5));
+        }
+    }
     class AccelWidget
     {
         private HudAPIv2 hud_handle_;
@@ -83,6 +101,18 @@ namespace Natomic.Accelerometer
             hud_handle_ = new HudAPIv2(OnHUDRegistered);
         }
 
+        private void ChangePos(Vector2D pos)
+        {
+            var sane_pos = CoordHelper.KeenCoordToSane(pos);
+
+            var bounds = CoordHelper.KeenCoordToSane(romiter_handle_.GetTextLength());
+            var txt_pos = new Vector2D(sane_pos.X + bounds.X / 2, sane_pos.Y + bounds.Y / 2);
+            romiter_handle_.Origin = CoordHelper.SaneToKeenCoord(txt_pos);
+
+            Conf.Position = pos;
+            pos_input_.Origin = pos;
+        }
+
         private void OnHUDRegistered()
         {
             try
@@ -91,12 +121,8 @@ namespace Natomic.Accelerometer
 
 
                 menu_ = new HudAPIv2.MenuRootCategory("Accelerometer", AttachedMenu: HudAPIv2.MenuRootCategory.MenuFlag.PlayerMenu, HeaderText: "Config");
-                pos_input_ = new HudAPIv2.MenuScreenInput(Text: "Position", Parent: menu_, Origin: Conf.Position, Size: romiter_handle_.GetTextLength(), OnSubmit: pos =>
-                {
-                    romiter_handle_.Origin = pos;
-                    Conf.Position = pos;
-                    pos_input_.Origin = pos;
-                }, InputDialogTitle: "[042.99 m/s²]");
+                pos_input_ = new HudAPIv2.MenuScreenInput(Text: "Position", Parent: menu_, Origin: Conf.Position, Size: romiter_handle_.GetTextLength(), OnSubmit: ChangePos
+                , InputDialogTitle: "X");
 
 
                 var force_cat = new HudAPIv2.MenuSubCategory(Text: "Unit", Parent: menu_, HeaderText: "Unit");
